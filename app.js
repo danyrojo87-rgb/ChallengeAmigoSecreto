e0ef0614c5cadb5cbbb3b661e768eec1abc8ac01
Daniel Muñoz <danyrojo87@gmail.com>
2025-09-15 08:01:39 -0300

    Actualiza app.js

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let ingresaAmigo = document.getElementById("amigo");
+    let amigo = ingresaAmigo.value;
+
+    if (amigo === "") {
+        alert("Por favor ingresa el nombre de un amigo");
+        return;
+    }
+    
+    amigos.push(amigo);
+    console.log(amigos);
+}
